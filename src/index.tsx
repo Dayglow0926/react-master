b8ccca0a7733fe6d875ee1725561ede55bd3bacd
Dayglow0926 <dayglow0926@gmail.com>
2023-05-31 02:08:37 +0900

    feat: themes 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,23 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { lightTheme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-const darkMode = {
-  textColor: "whitesmoke",
-  backgroundColor: "#111",
-};
-
-const lightMode = {
-  textColor: "#111",
-  backgroundColor: "whitesmoke",
-};
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightMode}>
+    <ThemeProvider theme={lightTheme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
