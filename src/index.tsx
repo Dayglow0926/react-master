80398c522322b8ab735b45f77906b0552f8b1eca
Dayglow0926 <dayglow0926@gmail.com>
2023-05-31 23:05:15 +0900

    feat: createBrowserRouter 사용 ( errorElement 추가 )

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
-import App from "./App";
 import { lightTheme } from "./theme";
+import { RouterProvider } from "react-router-dom";
+import router from "./Router";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,7 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={lightTheme}>
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
 );
